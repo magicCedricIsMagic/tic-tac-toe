a8071f373220e46aab84dc994b30e3eeb18c31a3
Cédric Gallon <cedricgallon@gmail.com>
2025-02-12 02:09:55 +0100

    prompt

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 import game from "/js/game.js"
 
-// game.init(prompt("Player one name"), prompt("Player two name"))
-game.init("Xavier", "Ophélie")
+game.init(prompt("Player one name"), prompt("Player two name"))
+// game.init("Xavier", "Ophélie")
