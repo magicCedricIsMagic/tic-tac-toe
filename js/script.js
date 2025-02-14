85a1f122be70e0e26369db33681b86ab28b11fd2
Cédric Gallon <cedricgallon@gmail.com>
2025-02-14 19:49:38 +0100

    No no name

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 import game from "/js/game.js"
 
-game.init(prompt("Player one name"), prompt("Player two name"))
+game.init(
+	prompt("Player one name") || "Player one",
+	prompt("Player two name") || "Player two"
+)
 // game.init("Xavier", "Ophélie")
